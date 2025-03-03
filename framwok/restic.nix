1890512579a4623d16cadf8e4b19f40b5a90e7c5
pilz0 <marie0@riseup.net>
2025-03-03 09:59:55 +0100

    now with sane restic config

--- framwok/restic.nix
+++ framwok/restic.nix
@@ -2,23 +2,29 @@
 {
   age.secrets.rcloneconfig = {
     file = ../secrets/rclone.age;
-    owner = "root";
-    group = "root";
+    owner = "marie";
+    group = "marie";
+  };
+  age.secrets.restic = {
+    file = ../secrets/restic.age;
+    owner = "marie";
+    group = "marie";
   };
   services.restic.backups = {
     onedrive = {
       rcloneConfigFile = config.age.secrets.rcloneconfig.path;
-      user = "root";
-      repository = "rclone:onedrive:/Buro/backup";
+      user = "marie";
+      repository = "rclone:onedrive:/backup/server";
       initialize = true; # initializes the repo, don't set if you want manual control
       passwordFile = config.age.secrets.restic.path;
       paths = [
         "/home/marie"
-        "/var/"
       ];
-      timerConfig = {
-        onCalendar = "sunday 11:00";
-      };
+      exclude = [
+        "*/.cache/*"
+        "*/.local/share/Trash/*"
+        "*/.steam/*"
+      ];
     };
   };
 }
