802fa0193715b44268b51f58afde850aae24b87b
pilz0 <48645439+pilz0@users.noreply.github.com>
2026-09-11 16:07:21 +0200

    wraf

--- machines/web1/time-machine.nix
+++ machines/web1/time-machine.nix
@@ -3,15 +3,24 @@
     enable = true;
     openFirewall = true;
     settings = {
+      global = {
+        "fruit:aapl" = "yes";
+        "fruit:model" = "MacSamba";
+        "fruit:nfs_aces" = "no";
+        "ea support" = "yes";
+        "vfs objects" = "catia fruit streams_xattr";
+      };
       "tm_share" = {
         "path" = "/srv/timemachine";
         "valid users" = "marie";
         "public" = "no";
         "writeable" = "yes";
         "force user" = "marie";
-        "fruit:aapl" = "yes";
         "fruit:time machine" = "yes";
         "vfs objects" = "catia fruit streams_xattr";
+        "spotlight" = "no";
+        "create mask" = "0600";
+        "directory mask" = "0700";
       };
     };
   };
